1ddfa5f12e831bc33debb3f9b494c10e9c8daf27
hemaspandanadhavuluri <hemaspandanadhavuluri@gmail.com>
2026-01-30 19:29:18 +0530

    Resolvong COrs

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ mongoose.connection.once('open', () => {
 });
 
 const corsOptions = {
-    origin: 'http://13.48.131.69:3000',
+    origin: 'http://13.48.131.69',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204
