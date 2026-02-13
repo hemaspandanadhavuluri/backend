9813fe376e40168dd0358ec363f6aee7275ea141
Charitha Savaram <svscharitha433@gmail.com>
2026-02-13 23:36:47 +0530

    changed ip address

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ mongoose.connection.once('open', () => {
 });
 
 const corsOptions = {
-    origin: 'http://13.48.131.69:3000',
+    origin: 'http://13.48.131.69',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     optionsSuccessStatus: 204
